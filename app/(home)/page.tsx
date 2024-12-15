7d3b9833cda3583027b959b5d523aea5c50d5720
evanqian2175 <evanqian2175@gmail.com>
2024-12-15 23:08:03 +1300

    test

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,17 +7,20 @@ export default function HomePage() {
       <h1 className="mb-4 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-blue-500 to-blue-950">
         HNZRP Staff Handbook
       </h1>
+
       <h1 className="mb-4 text-5xl font-extrabold text-[#bebebe]">
         discord.gg/hnzrp
       </h1>
+
       <h2 className="mb-4 text-3xl font-normal color text-[#6b6e6e]">
         Made with ❤️ by Aqxorus and TempNinjaMan
       </h2>
-      <img
-        src="/icon.png"
-        alt="HNZRP"
-        className="w-[320px] h-[320px] mx-auto"
-      />
+
+      <div className="my-8 relative w-[320px] h-[320px] mx-auto">
+        <div className="absolute inset-0 radial-blur"></div>
+        <img src="/icon.png" alt="HNZRP" className="opacity-[99.99999%]" />
+      </div>
+
       <div className="flex w-full justify-center items-center">
         <Callout className="w-1/2 text-left p-6" icon="" title="Version">
           v3
